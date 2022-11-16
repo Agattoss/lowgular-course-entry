a8d0c152a59b08c79a08c9bc12b269fb2e97e82f
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-16 21:49:58 +0100

    feat: display employee list (Task 1)

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,17 +1,17 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
   selector: 'employee-list',
   templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
   data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>("assets/data/employees.json")
-
   constructor(private _httpClient: HttpClient) {
   }
 }

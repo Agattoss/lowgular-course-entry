4a95e1f265e3fd01185e534f19c79262a7579d39
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-17 14:16:31 +0100

    feat: create PersonModel

---
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
+import {PersonModel} from "../../model/person.model";
+
+@Component({
+  selector: 'app-employees-faces',
+  templateUrl: './employees-faces.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeesFacesComponent {
+  constructor(private _employeeService: EmployeeService) {
+  }
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
+}

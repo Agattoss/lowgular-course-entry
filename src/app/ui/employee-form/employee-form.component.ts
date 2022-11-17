275abf6878319896cf3233491f820d316a2c924a
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-17 20:33:56 +0100

    feat: create Post request with alert (task 3.1 and 3.2)

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
+import { CreateEmployeeModel } from '../../model/create-employee.model';
 
 @Component({
   selector: 'employee-form',
@@ -10,9 +12,18 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup(
     {
-      name: new FormControl(null,[Validators.required]),
+      name: new FormControl(null, [Validators.required]),
       age: new FormControl(null, [Validators.min(0)]),
       salary: new FormControl(null, [Validators.min(0), Validators.required])
     });
+
+  constructor(private _employeeService: EmployeeService) {
+  }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+   return this._employeeService.create
+   ({name: form.name, age: form.age, salary: form.salary}).subscribe
+   ({complete: () => alert(`User was successfully added to the database. Name: ${this.employeeForm.value.name}, Age: ${this.employeeForm.value.age}, Salary: ${this.employeeForm.value.salary}`)} );
+  }
 
 }

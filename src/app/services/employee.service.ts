474773621d4ce4028f3dd3c14b34cf41a2ec7aad
Agata <108717863+Agattoss@users.noreply.github.com>
2022-11-23 17:44:34 +0100

    feat: Display employee data on details page, add "details" button

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,37 +1,60 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
+import { ApiResponse } from './api.response';
+import { EmployeeResponse } from './employee.response';
 import { CreateEmployeeModel } from '../model/create-employee.model';
-import {ApiResponse} from "./api.response";
-import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
-  constructor(private _httpClient: HttpClient) {
-  }
+  constructor(private _httpClient: HttpClient) {}
   getAll(): Observable<PersonModel[] | null> {
-    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(
-      map((response:ApiResponse<EmployeeResponse[]>) : PersonModel[] => {
-        return response.data.map(
-          (employeeResponse:EmployeeResponse)=> {
-            return{
+    return this._httpClient
+      .get<ApiResponse<EmployeeResponse[]>>(
+        'https://dummy.restapiexample.com/api/v1/employees'
+      )
+      .pipe(
+        map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
+          return response.data.map((employeeResponse: EmployeeResponse) => {
+            return {
               name: employeeResponse.employee_name,
               personalNumber: employeeResponse.id,
               img: employeeResponse.profile_image,
-              mail: employeeResponse.employee_name + "@lowgular.io"
-            }
-          }
-        )
-    })
-    )
+              mail: employeeResponse.employee_name + '@lowgular.io',
+            };
+          });
+        })
+      );
   }
   create(employee: CreateEmployeeModel): Observable<any> {
-    return this._httpClient.post<CreateEmployeeModel>('https://dummy.restapiexample.com/api/v1/create', employee)
+    return this._httpClient.post<CreateEmployeeModel>(
+      'https://dummy.restapiexample.com/api/v1/create',
+      employee
+    );
   }
 
   delete(id: string): Observable<any> {
-    return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/' + id);
+    return this._httpClient.delete(
+      'https://dummy.restapiexample.com/api/v1/delete/' + id
+    );
+  }
 
+  getOne(id: string): Observable<PersonModel> {
+    return this._httpClient
+      .get<ApiResponse<EmployeeResponse>>(
+        'https://dummy.restapiexample.com/api/v1/employee/' + id
+      )
+      .pipe(
+        map(
+          (response): PersonModel => ({
+            name: response.data.employee_name,
+            personalNumber: response.data.id,
+            img: response.data.profile_image,
+            mail: response.data.employee_name + '@lowgular.io',
+          })
+        )
+      );
   }
 }
--- src/app/ui/employee-detail/employee-detail.component.ts
+++ src/app/ui/employee-detail/employee-detail.component.ts
@@ -4,8 +4,11 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { EmployeeDetailParamsModel } from '../../model/employee-detail-params.model';
+import { PersonModel } from '../../model/person.model';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee-detail',
@@ -18,5 +21,12 @@ export class EmployeeDetailComponent {
     this._activatedRoute.params.pipe(
       map((params) => ({ id: params['id'], name: params['name'] }))
     );
-  constructor(private _activatedRoute: ActivatedRoute) {}
+  readonly details$: Observable<PersonModel> = this._activatedRoute.params.pipe(
+    switchMap((data) => this._employeeService.getOne(data['id']))
+  );
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _employeeService: EmployeeService
+  ) {}
 }
